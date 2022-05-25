61fffaf9a8b9033f421a6e7403fd93b9c89ca523
tijmenvanetten <tijmenettenvan@gmail.com>
2022-05-25 10:37:47 +0200

    add feature

--- preprocessing.py
+++ preprocessing.py
@@ -3,16 +3,21 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import GroupShuffleSplit
 
+def preprocess(df):
+    # something
+    df = drop_irrelevant_features(df)
+    # Add engineered features
+    df = add_datetime_features(df)
+    df = add_price_order(df)
+    # Normalize features
+    df = normalize(df)
+    return df
 
 def load_train(train_path, val_split=False):
     df = pd.read_csv(train_path, parse_dates=['date_time'])
     df['score'] = calculate_score(df)
 
-    df = drop_irrelevant_features(df)
-    # Add engineered features
-    df = add_features(df)
-    # Normalize features
-    df = normalize(df)
+    df = preprocess(df)
 
     # remove columns missing in test data
     X, y = df.drop(['date_time', 'position', 'score', 'click_bool', 'booking_bool', 'gross_bookings_usd'], axis=1), df[['srch_id', 'score']]
@@ -24,8 +29,7 @@ def load_train(train_path, val_split=False):
 
 def load_test():
     df = pd.read_csv("data/test_set_VU_DM.csv", parse_dates=['date_time'])
-    df = add_features(df)
-    df = drop_irrelevant_features(df)
+    df = preprocess(df)
     X = df.drop(['date_time'], axis=1)
     return X
 
@@ -41,9 +45,6 @@ def train_val_split(X, y, groups, val_size=.7):
 
     return X_train, y_train, X_val, y_val
 
-def add_features(df):
-    df = add_datetime_features(df)
-    return df
 
 def drop_irrelevant_features(df):
     feats = ['comp1_rate', 'comp1_inv', 'comp1_rate_percent_diff', 'comp3_rate_percent_diff', 'comp4_rate', 'comp4_inv', 'comp4_rate_percent_diff', 'comp6_rate', 'comp6_inv', 'comp6_rate_percent_diff', 'comp7_rate', 'comp7_inv', 'comp7_rate_percent_diff']
@@ -64,4 +65,8 @@ def add_datetime_features(df):
     df['month'] = df['date_time'].dt.month
     df['dayofweek'] = df['date_time'].dt.dayofweek
     df['hour'] = df['date_time'].dt.hour
+    return df
+
+def add_price_order(df):
+    df['price_order'] = df.groupby('srch_id')['price_usd'].apply(lambda x: np.argsort(x)[::-1]).values
     return df
